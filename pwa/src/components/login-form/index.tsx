d4f41e35a49e0bee3f26c1c13ddeb3dce8198c8a
develop.kosarev <develop.kosarev@gmail.com>
2025-05-25 21:33:37 +0200

    Add signin

--- pwa/src/components/login-form/index.tsx
+++ pwa/src/components/login-form/index.tsx
@@ -47,10 +47,7 @@ export default function LoginForm() {
               <input
                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                 id="email"
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
-                required
+                type="email"  name="email" placeholder="Enter your email address" required
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -61,11 +58,7 @@ export default function LoginForm() {
               <input
                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                 id="password"
-                type="password"
-                name="password"
-                placeholder="Enter password"
-                required
-                minLength={6}
+                type="password" name="password" placeholder="Enter password" required minLength={6}
               />
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
