5554e5f88f98a1c569f5772ab3aa2c661a19ab43
develop.kosarev <develop.kosarev@gmail.com>
2025-07-10 17:02:40 +0200

    Fix

--- pwa/app/profile-client/page.tsx
+++ pwa/app/profile-client/page.tsx
@@ -1,40 +1,40 @@
 'use client';
 
-import ClientGreetingButton from "../../src/components/greeting";
-import { useSession, signOut } from 'next-auth/react';
+//import ClientGreetingButton from "../../src/components/greeting";
+//import { useSession, signOut } from 'next-auth/react';
 import { useEffect } from "react";
 
 export default function Page() {
-  const { data: session, status } = useSession();
+  //const { data: session, status } = useSession();
 
-  console.log('session')
-  console.log(session)
+  //console.log('session')
+  //console.log(session)
 
-  useEffect(() => {
-    // Проверка на ошибку или недействительную сессию
-    if (status === 'unauthenticated') {
-      console.log("Сессия недействительна или истекла, выходим...");
-      signOut({ callbackUrl: "/" });
-    }
-    //if (status === 'authenticated' && session.error) {
-    if (status === 'authenticated') {
-      signOut({ callbackUrl: "/" });
-    }
-    console.log('status: ' + status)
-  }, [status]);
+  //useEffect(() => {
+  //  // Проверка на ошибку или недействительную сессию
+  //  if (status === 'unauthenticated') {
+  //    console.log("Сессия недействительна или истекла, выходим...");
+  //    signOut({ callbackUrl: "/" });
+  //  }
+  //  //if (status === 'authenticated' && session.error) {
+  //  if (status === 'authenticated') {
+  //    signOut({ callbackUrl: "/" });
+  //  }
+  //  console.log('status: ' + status)
+  //}, [status]);
 
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-        <div>
-          <ClientGreetingButton />
-        </div>
+        {/*<div>*/}
+        {/*  <ClientGreetingButton />*/}
+        {/*</div>*/}
 
-        <p>Profile</p>
-        {status === 'loading' ?
-          <div>loading...</div> :
-          <pre>{JSON.stringify(session, null, 2)}</pre>
-        }
+        {/*<p>Profile</p>*/}
+        {/*{status === 'loading' ?*/}
+        {/*  <div>loading...</div> :*/}
+        {/*  <pre>{JSON.stringify(session, null, 2)}</pre>*/}
+        {/*}*/}
 
       </div>
     </div>
