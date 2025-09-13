55e6ddd90899192e6ae5ce03b928b09c22cbbe0c
frontendDevNat <39639364+frontendDevNat@users.noreply.github.com>
2025-09-13 22:49:13 +0200

    add recommendation page

---
+++ pwa/src/app/[locale]/(customers)/personal-account/recommendations/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex items-center justify-center">
+        Client Recommendations Page
+      </div>
+    </main>
+  );
+}
